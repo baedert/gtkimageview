bacccab996c9f7ad043bbc78c88ae6b4333eed35
Timm Bäder <mail@baedert.org>
2016-03-08 11:14:38 +0100

    imageview: Ref input stream before using it
    And don't unref it twice.
    
    Fixes #5

--- gtkimageview.c
+++ gtkimageview.c
@@ -2236,8 +2236,6 @@ gtk_image_view_load_image_from_stream (GtkImageView *image_view,
 
   if (!error)
     gtk_image_view_replace_animation (image_view, result, scale_factor);
-
-  g_object_unref (input_stream);
 }
 
 static void
@@ -2329,8 +2327,7 @@ gtk_image_view_load_from_file_async (GtkImageView        *image_view,
 
   task_data = g_slice_new (LoadTaskData);
   task_data->scale_factor = scale_factor;
-  task_data->source = file;
-  g_object_ref (file);
+  task_data->source = g_object_ref (file);
 
   task = g_task_new (image_view, cancellable, callback, user_data);
   g_task_set_task_data (task, task_data, (GDestroyNotify)free_load_task_data);
@@ -2390,7 +2387,7 @@ gtk_image_view_load_from_stream_async (GtkImageView        *image_view,
 
   task_data = g_slice_new (LoadTaskData);
   task_data->scale_factor = scale_factor;
-  task_data->source = input_stream;
+  task_data->source = g_object_ref (input_stream);
 
   task = g_task_new (image_view, cancellable, callback, user_data);
   g_task_set_task_data (task, task_data, (GDestroyNotify)free_load_task_data);
